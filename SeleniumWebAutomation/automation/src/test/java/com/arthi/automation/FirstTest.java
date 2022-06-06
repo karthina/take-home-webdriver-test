947783b85c7a11b8353d3cee2e0dae61838264de
Manivannan Ranganathan <mranga@Manis-Pro-Mac.local>
2022-06-06 15:51:46 -0700

    upgraded the jdk version to 11

--- SeleniumWebAutomation/automation/src/test/java/com/arthi/automation/FirstTest.java
+++ SeleniumWebAutomation/automation/src/test/java/com/arthi/automation/FirstTest.java
@@ -11,29 +11,29 @@ public class FirstTest extends TestBase {
         assertEquals("Test", "Test");
     }
 
-    @Test(priority=0, description=" verify the login test with valid username and password")
+    @Test(priority=0, description=" verify the login test2 with valid username and password")
 	public void valid_Username_Password_Test2() {
-        assertEquals("Test", "Test");
+        assertEquals("Test2", "Test2");
     }
 
-    @Test(priority=0, description=" verify the login test with valid username and password")
+    @Test(priority=0, description=" verify the login test3 with valid username and password")
 	public void valid_Username_Password_Test3() {
-        assertEquals("Test", "Test");
+        assertEquals("Test3", "Test3");
     }
 
-    @Test(priority=0, description=" verify the login test with valid username and password")
+    @Test(priority=0, description=" verify the login test4 with valid username and password")
 	public void valid_Username_Password_Test4() {
-        assertEquals("Test", "Test");
+        assertEquals("Test4", "Test4");
     }
 
-    @Test(priority=0, description=" verify the login test with valid username and password")
+    @Test(priority=0, description=" verify the login test5 with valid username and password")
 	public void valid_Username_Password_Test5() {
-        assertEquals("Test", "Test");
+        assertEquals("Test5", "Test5");
     }
 
-    @Test(priority=0, description=" verify the login test with valid username and password")
+    @Test(priority=0, description=" verify the login test6 with valid username and password")
 	public void valid_Username_Password_Test6() {
-        assertEquals("Test", "Test");
+        assertEquals("Test6", "Test6");
     }
 
 }
