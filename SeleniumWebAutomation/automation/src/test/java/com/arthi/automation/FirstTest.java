622d303fe77d213d527fe217ac327db7de4ae10b
Manivannan Ranganathan <mranga@Manis-Pro-Mac.local>
2022-06-06 15:33:16 -0700

    Updated sngle test class

---
+++ SeleniumWebAutomation/automation/src/test/java/com/arthi/automation/FirstTest.java
@@ -0,0 +1,39 @@
+package com.arthi.automation;
+
+import org.testng.annotations.Test;
+
+import com.arthi.automation.common.TestBase;
+
+public class FirstTest extends TestBase {
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test() {
+        assertEquals("Test", "Test");
+    }
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test2() {
+        assertEquals("Test", "Test");
+    }
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test3() {
+        assertEquals("Test", "Test");
+    }
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test4() {
+        assertEquals("Test", "Test");
+    }
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test5() {
+        assertEquals("Test", "Test");
+    }
+
+    @Test(priority=0, description=" verify the login test with valid username and password")
+	public void valid_Username_Password_Test6() {
+        assertEquals("Test", "Test");
+    }
+
+}
